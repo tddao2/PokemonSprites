a9dabe2ff1109109b587f7ff4576cee2a93a8327
Cristian Dao <cdao@cougarnet.uh.edu>
2022-08-01 02:38:00 -0500

    Pokemon lists horizontally

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const baseURL =
     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 for (let i = 1; i < 151; i++) {
+    const pokemon = document.createElement('div');
+    const label = document.createElement('span');
+    label.innerHTML = `#${i}`;
     const newImg = document.createElement('img');
     newImg.src = `${baseURL}${i}.png`;
-
-    container.appendChild(newImg);
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
 }
