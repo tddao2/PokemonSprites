fa4f1d2b89c1384859b9d368bf2f05556100a4f5
Cristian Dao <cdao@cougarnet.uh.edu>
2022-08-01 02:42:33 -0500

    Pokemon lists vertically

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const container = document.querySelector('#container');
 const baseURL =
     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-for (let i = 1; i < 151; i++) {
+for (let i = 1; i <= 151; i++) {
     const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
     const label = document.createElement('span');
     label.innerHTML = `#${i}`;
     const newImg = document.createElement('img');
