a8d421362f2b452fb7049a60ce0d2a95f7544f9c
Cristian Dao <cdao@cougarnet.uh.edu>
2022-08-01 02:31:14 -0500

    My first pokemon

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container');
+const newImg = document.createElement('img');
+newImg.src =
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+container.appendChild(newImg);
