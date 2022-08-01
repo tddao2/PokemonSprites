52d83977b2d67ef877d1daaf6aef96ca101feb82
Cristian Dao <cdao@cougarnet.uh.edu>
2022-08-01 02:34:48 -0500

    My next 10 pokemons

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
 const container = document.querySelector('#container');
-const newImg = document.createElement('img');
-newImg.src =
-    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
-container.appendChild(newImg);
+const baseURL =
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+for (let i = 1; i < 10; i++) {
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`;
+
+    container.appendChild(newImg);
+}
